ba5e8f927e6a2e0f402decbbbadd3fbe6ea5918f
iqbal-77 <iqbalather77@gmail.com>
2026-01-29 01:51:43 +0500

    function array commit

--- Array/Array1.cpp
+++ Array/Array1.cpp
@@ -1,42 +1,92 @@
 #include <iostream>
-#include <climits> // INT_MIN ke liye
 using namespace std;
 
-void findInter(int arr1[], int arr2[], int size1, int size2){
+// Reverse an Array function
+void reverseArray(int arr[], int size){
+    int start = 0;
+    int end = size - 1;
+    for(int i = 0; i < size; i++){
+        if(start < end){
+            swap(arr[start], arr[end]); 
+        }
+        start++;
+        end--;
+    }
+}
+
+// unique Values
+
+void uniqueValue(int arr[], int size){
     
-    cout << "Intersection values: ";
-    
-    for(int i = 0; i < size1; i++){
-        
-        for(int j = 0; j < size2; j++){
-            
-            // Step 1: Match Dhoondo
-            if(arr1[i] == arr2[j]){
-                
-                // Mil gaya! Print karo
-                cout << arr1[i] << " ";
-                
-                // Step 2: "Mark" kar do (Taake dobara count na ho)
-                arr2[j] = INT_MIN; 
-                
-                // Step 3: Break karo (Ek number ek hi baar match hoga)
-                break; 
+    for(int i = 0; i < size; i++){
+        bool isUnique = true;
+        for(int j = 0; j < size; j++){
+            if(i != j && arr[i] == arr[j]){
+                isUnique = false;
+                break;
             }
+        }
+        if(isUnique){
+            cout << arr[i] << " ";
         }
     }
 }
 
+void interValue(int arr1[], int arr2[], int size1, int size2){
+    for(int i = 0; i < size1; i++){
+        bool found = false;
+        for(int j = 0; j < size2; j++){
+            if(arr1[i] == arr2[j]){
+                found = true;
+            }
+        }
+        if(found){
+            cout << arr1[i] << " ";
+        }
+    }
+}
+
+void foundValue(int arr[], int size, int target){
+    int idx = 0;
+    
+    for(int i = 0; i < size; i++){
+        if(arr[i] == target){
+            target = arr[i];
+            idx = i;
+        }      
+    }
+    cout << idx << endl;
+}
+
+// largest value find
+int largest(int arr[], int size){
+        // code here
+        int largest = INT_MIN;
+        for(int i = 0; i < size; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+                
+            }
+        }
+        return largest;
+    }
+
 int main(){
-    int size1 = 6;
-    int size2 = 4;
-    // Thora example change kiya taake intersection dikhay
-    int arr1[] = {1, 1, 2, 2, 3, 5}; 
-    int arr2[] = {1, 2, 2, 3};
-    
-    // Logic:
-    // arr1 ka pehla '1' -> arr2 ke '1' se match karega. (arr2 ka 1 ab khatam)
-    // arr1 ka dusra '1' -> arr2 mn ab koi 1 nahi bacha. Skip hoga.
-    
-    findInter(arr1, arr2, size1, size2);
+    int size = 4;
+    int target = 9;
+    int arr[] = {1, 5, 8, 9};
+    cout << largest(arr, size);
+    // foundValue(arr, size, target);
+    // int size1 = 4;
+    // int size2 = 2;
+    // int arr1[] = {1, 1, 2, 3};
+    // int arr2[] = {2, 3};
+    // interValue(arr1, arr2, size1, size2);
+
+    // uniqueValue(arr, size);
+    // reverseArray(arr, size);
+    // for(int i = 0; i < size; i++){
+    //     cout << arr[i] << " ";
+    // }
     return 0;
 }
--- Array/array.cpp
+++ Array/array.cpp
@@ -68,237 +68,239 @@ int main(){
     int arr[] = {1, 2, 1, 3, 4};
     uniqueValue(arr, size);
 
-    int size1 = 4;
-    int size2 = 2;
-    int arr1[] = {1, 2, 3, 4};
-    int arr2[] = {5, 4};
-    findInter(arr1, arr2, size1, size2);
-    cout << uniqueValue(arr, size)<< endl;
-
-   
-    
-    
-    for(int i = 0; i < size; i++){
-        bool isUnique = true;
-        for(int j = 0; j  < size; j++){
-            if(j != i && arr[i] == arr[j]){
-                 isUnique = false;
-                 break;
-            }  
-        }
-         if(isUnique){
-        cout << arr[i] << " ";
-        }
-      
-      
-    }
-   
     return 0;
 }
-swapValue(arr, size);
-    for(int i = 0; i< size; i++){
-        cout << arr[i] << " ";
-    }
-void changeArr(int arr[], int size){
-    cout << "in func\n";
-    for(int i = 0; i < size; i++){
-        arr[i] *= 2;
-        cout << arr[i] << endl;
-    }
+//     int size1 = 4;
+//     int size2 = 2;
+//     int arr1[] = {1, 2, 3, 4};
+//     int arr2[] = {5, 4};
+//     findInter(arr1, arr2, size1, size2);
+//     cout << uniqueValue(arr, size)<< endl;
+
+   
     
-}
-void changeX(int n){
-    n += 10;
-    cout << "in function\n";
-    cout << n << endl;
-
-}
-
-int findIdx(int arr[], int target, int size){
+    
+//     for(int i = 0; i < size; i++){
+//         bool isUnique = true;
+//         for(int j = 0; j  < size; j++){
+//             if(j != i && arr[i] == arr[j]){
+//                  isUnique = false;
+//                  break;
+//             }  
+//         }
+//          if(isUnique){
+//         cout << arr[i] << " ";
+//         }
+      
+      
+//     }
    
-    for(int i = 0; i < size; i++){
-        if(arr[i] == target){
-            return i;
-        }
-        
-    }
-   return -1;
-   
-}
-void reverseArr(int arr[], int size){
-   int start = 0, end = size - 1;
-   while(start < end){
-    swap(arr[start], arr[end]);
-   start++;
-   end--;
-   }
-}
-int sumArray(int arr[], int size){
-    int sum = 1;
-    for(int i = 0; i < size; i++){
-        sum *= arr[i];
-    }
-    return sum;
-}
-void swapValue(int arr[], int size){
-    int start = 0;
-    int end = 0;
-    int smallest = INT_MAX;
-    int largest = INT_MIN;
-    for(int i = 0; i < size; i++){
-        if(arr[i] < smallest){
-            smallest = arr[i];
-            start = i;
-        }
-        if(arr[i] > largest){
-            largest = arr[i];
-            end = i;
-        }
-    }
-    // cout << smallest << largest << endl;
-    swap(arr[start], arr[end]);
-}
+//     return 0;
+// }
+// swapValue(arr, size);
+//     for(int i = 0; i< size; i++){
+//         cout << arr[i] << " ";
+//     }
+// void changeArr(int arr[], int size){
+//     cout << "in func\n";
+//     for(int i = 0; i < size; i++){
+//         arr[i] *= 2;
+//         cout << arr[i] << endl;
+//     }
+    
+// }
+// void changeX(int n){
+//     n += 10;
+//     cout << "in function\n";
+//     cout << n << endl;
 
-void uniqueValue(int arr[], int size){
-    int x = 0;
-    int idx = 0;
-    for(int i = 0; i < size; i++){
+// }
+
+// int findIdx(int arr[], int target, int size){
+   
+//     for(int i = 0; i < size; i++){
+//         if(arr[i] == target){
+//             return i;
+//         }
         
-        for(int j = 1; j = i; j++){
-            arr[idx] != arr[j];
-            x = idx;
-           break;
-        }
-        idx += 1;
-       cout << arr[x] << " ";
+//     }
+//    return -1;
+   
+// }
+// void reverseArr(int arr[], int size){
+//    int start = 0, end = size - 1;
+//    while(start < end){
+//     swap(arr[start], arr[end]);
+//    start++;
+//    end--;
+//    }
+// }
+// int sumArray(int arr[], int size){
+//     int sum = 1;
+//     for(int i = 0; i < size; i++){
+//         sum *= arr[i];
+//     }
+//     return sum;
+// }
+// void swapValue(int arr[], int size){
+//     int start = 0;
+//     int end = 0;
+//     int smallest = INT_MAX;
+//     int largest = INT_MIN;
+//     for(int i = 0; i < size; i++){
+//         if(arr[i] < smallest){
+//             smallest = arr[i];
+//             start = i;
+//         }
+//         if(arr[i] > largest){
+//             largest = arr[i];
+//             end = i;
+//         }
+//     }
+//     // cout << smallest << largest << endl;
+//     swap(arr[start], arr[end]);
+// }
+
+// void uniqueValue(int arr[], int size){
+//     int x = 0;
+//     int idx = 0;
+//     for(int i = 0; i < size; i++){
         
-    }
+//         for(int j = 1; j = i; j++){
+//             arr[idx] != arr[j];
+//             x = idx;
+//            break;
+//         }
+//         idx += 1;
+//        cout << arr[x] << " ";
+        
+//     }
     
      
-}
+// }
 
-int main(){
-    int size = 3;
-    int arr[] = {1, 1, 2, 3, 4};
-    uniqueValue(arr, size);
+// int main(){
+//     int size = 3;
+//     int arr[] = {1, 1, 2, 3, 4};
+//     uniqueValue(arr, size);
     
-    reverseArr(arr, size);
+//     reverseArr(arr, size);
     
-    for(int i = 0; i < size; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+//     for(int i = 0; i < size; i++){
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
 
-    int target = 8;
-    cout << findIdx(arr, target, size);
-    int x = 10;
-    changeX(x);
-    // here X is change is not change in main function because it is primitive data type.
-    cout << "in main\n";
-    cout << x << endl;
-    int arr[] = {1, 2, 3};
-    changeArr(arr, 3);
+//     int target = 8;
+//     cout << findIdx(arr, target, size);
+//     int x = 10;
+//     changeX(x);
+//     // here X is change is not change in main function because it is primitive data type.
+//     cout << "in main\n";
+//     cout << x << endl;
+//     int arr[] = {1, 2, 3};
+//     changeArr(arr, 3);
 
-    cout << "main\n";
-    for(int i = 0; i < 3; i++){
-        cout << arr[i]<< endl;
-    }
-
-
+//     cout << "main\n";
+//     for(int i = 0; i < 3; i++){
+//         cout << arr[i]<< endl;
+//     }
 
 
-    int size = 6;
-    int nums[] = {100, 19, 98, -13, 21, 78};
-    // // find largest, smallest value
-    int smallest = INT_MAX;
-    int largest = INT_MIN;
-    int idx = 0;
-    int idx2 = 0;
 
-    for(int i = 0; i < size; i++){
-        if(nums[i] < smallest){
-            smallest = nums[i];
-            idx = i;
-        }
-        if(nums[i] > largest){
-            largest = nums[i];
-            idx2 = i;
-        }
-    }
-    cout << idx<< endl;
-    cout << idx2;
 
-    for(int i = 0; i < size; i++){
-        smallest = min(nums[i], smallest);
-        largest = max(nums[i], largest);
-        if(nums[i] == smallest){
-            idx = i;
-        }
-        if(nums[i] == largest){
-            idx2  = i;
-        }
-    }
-    cout << "smallest value index = " << idx << endl;
-    cout << "largest value index = "  << idx2 << endl;
-    for(int i = 0; i < size; i++){
-        smallest = min(nums[i], smallest);
-        idx = nums[i];
-        largest = max(nums[i], largest);
-    }
+//     int size = 6;
+//     int nums[] = {100, 19, 98, -13, 21, 78};
+//     // // find largest, smallest value
+//     int smallest = INT_MAX;
+//     int largest = INT_MIN;
+//     int idx = 0;
+//     int idx2 = 0;
+
+//     for(int i = 0; i < size; i++){
+//         if(nums[i] < smallest){
+//             smallest = nums[i];
+//             idx = i;
+//         }
+//         if(nums[i] > largest){
+//             largest = nums[i];
+//             idx2 = i;
+//         }
+//     }
+//     cout << idx<< endl;
+//     cout << idx2;
+
+//     for(int i = 0; i < size; i++){
+//         smallest = min(nums[i], smallest);
+//         largest = max(nums[i], largest);
+//         if(nums[i] == smallest){
+//             idx = i;
+//         }
+//         if(nums[i] == largest){
+//             idx2  = i;
+//         }
+//     }
+//     cout << "smallest value index = " << idx << endl;
+//     cout << "largest value index = "  << idx2 << endl;
+//     for(int i = 0; i < size; i++){
+//         smallest = min(nums[i], smallest);
+//         idx = nums[i];
+//         largest = max(nums[i], largest);
+//     }
     
 
 
-    cout << "smallest = " << smallest << endl;
-    cout << "largest = " << largest << endl;
+//     cout << "smallest = " << smallest << endl;
+//     cout << "largest = " << largest << endl;
 
 
-    int smallest = INT_MAX;
-    for(int i = 0; i < size ; i++){
-    smallest = min(nums[i], smallest);
-    }
+//     int smallest = INT_MAX;
+//     for(int i = 0; i < size ; i++){
+//     smallest = min(nums[i], smallest);
+//     }
 
-    // find smallest through loop
-    for(int i = 0; i < size; i++){
-        if(nums[i] < smallest){
-            smallest = nums[i];
-        }
-    }
-    cout << "smallest = "  << smallest << endl;
+//     // find smallest through loop
+//     for(int i = 0; i < size; i++){
+//         if(nums[i] < smallest){
+//             smallest = nums[i];
+//         }
+//     }
+//     cout << "smallest = "  << smallest << endl;
 
 
 
-    int marks[size];
-    for(int i = 0; i < size; i++){
+//     int marks[size];
+//     for(int i = 0; i < size; i++){
         
-        cin >> marks[i];
-    }
+//         cin >> marks[i];
+//     }
 
 
 
-    // loop 0 to size-1
-    for(int i = 0; i < size; i++){
-        cout << marks[i] << endl;
-    }
+//     // loop 0 to size-1
+//     for(int i = 0; i < size; i++){
+//         cout << marks[i] << endl;
+//     }
 
 
-    // calculate the size of array
-    cout << sizeof(marks) / sizeof(int) << endl;
+//     // calculate the size of array
+//     cout << sizeof(marks) / sizeof(int) << endl;
 
 
-    // access the index
-    marks[0] = 100;
-    cout << marks[0] << endl;
-    cout << marks[1] << endl;
-    cout << marks[2] << endl;
-    cout << marks[3] << endl;
-    cout << marks[4] << endl;
+//     // access the index
+//     marks[0] = 100;
+//     cout << marks[0] << endl;
+//     cout << marks[1] << endl;
+//     cout << marks[2] << endl;
+//     cout << marks[3] << endl;
+//     cout << marks[4] << endl;
     
-    // 0 to size -1
+//     // 0 to size -1
 
-    // if we write index which is not exist it will through garbge value.
-    cout << marks[5] << endl;
+//     // if we write index which is not exist it will through garbge value.
+//     cout << marks[5] << endl;
 
-    double price[] = {99.99 , 105.99, 67.99};
+//     double price[] = {99.99 , 105.99, 67.99};
 
-    return 0;
-}
+//     return 0;
+// }
